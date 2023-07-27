09a6eb90f22116fd7db2b87ca53aee00a54072b1
nxtrm <nextrem_p@proton.me>
2023-07-27 19:10:29 +0300

    Metadata extraction fix, Assign ID to each quote

--- Database/database_creator.py
+++ Database/database_creator.py
@@ -2,34 +2,36 @@ import re
 import json
 
 def parse_quotes(input_text):
-    # Define the regular expression pattern to split quotes
+
+    counter = 0
+
     separator = r"\n==========\n"
-    
-    # Split the input text into individual quotes using re.split()
+
     quotes = re.split(separator, input_text)
-    
-    # Initialize an empty list to store the extracted data
+
     quotes_data = []
     
     for quote in quotes:
         if not quote.strip():
             continue
-        
-        # Extract the relevant information from each quote
+
         data = {}
         lines = quote.strip().split("\n")
         
+        data["id"] = counter
+
         data['BookName'] = lines[0].strip()
-        #Extract author
         data['Author'] = lines[0][(lines[0].find("(") + 1):(lines[0].find(")"))]
         data['DateAdded'] = lines[1][(lines[1].find("| Added on") +11) :]
-        data['Quote'] = ''
+        data['Quote'] = quote[quote.find("\n\n"): quote.find("==========")]
     
 
         # Remove extra spaces from the quote
         data['Quote'] = data['Quote'].strip()
 
         quotes_data.append(data)
+
+        counter = counter+1
     
     return quotes_data
 
