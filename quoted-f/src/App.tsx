4b5a63622179458ab8742c8037a7385f73f5c384
nxtrm <nextrem_p@proton.me>
2023-08-02 20:03:52 +0300

    Started a total redesign of the homepage

--- quoted-f/src/App.tsx
+++ quoted-f/src/App.tsx
@@ -1,12 +1,18 @@
-import { Badge, Box, HStack, Spinner, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  GridItem,
+  HStack,
+  Spinner,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import "./App.css";
 import BookInfo from "./Components/BookInfo";
 import EditButton from "./Components/EditButton";
-import LikeButton from "./Components/LikeButton";
+import LikeComponent from "./Components/LikeComponent";
 import NextButton from "./Components/NextButton";
 import useQuote from "./hooks/useQuotes";
-import LikeCount from "./Components/LikeCount";
-import LikeComponent from "./Components/LikeComponent";
 
 function App() {
   const { data: quote, isLoading } = useQuote();
@@ -14,27 +20,41 @@ function App() {
   if (isLoading) return <Spinner />;
 
   return (
-    <Box className="centered-container">
-      <VStack>
-        <HStack>
-          <Text maxWidth={500} fontSize={20}>
-            {quote?.Quote}
-          </Text>
+    <Grid
+      h="200px"
+      templateRows="repeat(2, 1fr)"
+      templateColumns="repeat(6, 1fr)"
+      gap={4}
+    >
+      <GridItem rowSpan={2} colSpan={1} bg="tomato"></GridItem>
+      <GridItem colSpan={3}>
+        <Text maxWidth={500} fontSize={20}>
+          {quote?.Quote}
+        </Text>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <VStack>
           <BookInfo bookName={quote?.BookName} authorName={quote?.AuthorName} />
-        </HStack>
-        <HStack>
-          <Text textColor={"gray.600"} fontSize={"15px"}>
-            {quote?.DateAdded}
-          </Text>
           <Box marginX={4}>
             <EditButton />
             <LikeComponent />
             <NextButton />
           </Box>
-          {/* <DeleteButton /> */}
-        </HStack>
-      </VStack>
-    </Box>
+        </VStack>
+      </GridItem>
+      <GridItem colSpan={5} bg="tomato"></GridItem>
+    </Grid>
+
+    //     <VStack>
+    //       <HStack>
+    //       </HStack>
+    //       <HStack>
+    //         <Text textColor={"gray.600"} fontSize={"15px"}>
+    //           {quote?.DateAdded}
+    //         </Text>
+    //         {/* <DeleteButton /> */}
+    //       </HStack>
+    //     </VStack>
   );
 }
 
