8b7c2899d203fcebd2d5ae6b18ab9b88f1821539
nxtrm <nextrem_p@proton.me>
2023-08-10 16:18:04 +0300

    Implemented the DeleteButton functionality

---
+++ quoted-f/src/hooks/useDelete.tsx
@@ -0,0 +1,12 @@
+import { useQuery } from "react-query";
+import APIClient from "../services/api-client";
+import { Quote } from "./quoteProvider";
+
+const apiClient = new APIClient<Quote>("/delete/");
+
+const useDelete = (slug: string) =>
+  useQuery({
+    queryKey: ["quote", slug],
+    queryFn: () => apiClient.get(slug),
+  });
+export default useDelete;
