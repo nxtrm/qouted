8d43632497dfa2cefcec119d1ac7d113787bd0d2
nxtrm <nextrem_p@proton.me>
2024-03-07 15:48:15 +0200

    Simplified the QuoteCard component

--- quoted-f/src/Components/QuoteCard.tsx
+++ quoted-f/src/Components/QuoteCard.tsx
@@ -1,61 +1,28 @@
 import { Box, Card, CardBody, HStack, Heading, Spacer, Text } from "@chakra-ui/react";
-import { useState } from "react";
-import { FaBookOpen, FaQuoteLeft } from "react-icons/fa";
-import useDislike from "../hooks/useDislike";
-import useLike from "../hooks/useLike";
-import LikeButton from "./LikeButton";
 
 interface Props {
+    id:string
     type:string
     text:string
     alttext:string
-    isLiked:boolean
-    id:string
+    icon:React.ReactNode
+
 }
 
-const QuoteCard = ({type,text,alttext,isLiked, id}:Props) => {
+const QuoteCard = ({type,text,alttext, id, icon}:Props) => {
     //Replace this later
-    const [liked, setLiked] = useState(false);
-    const { likeQuote } = useLike();
-    const { dislikeQuote } = useDislike();
-    
-    if (isLiked) {
-        setLiked(true)
-    }
 
-    const handleLikeClick = async () => {
-        if (!liked) {
-          await likeQuote(id);
-          setLiked(true);
-        } else {
-          await dislikeQuote(id);
-          setLiked(false);
-        }
-      };
 
     return(
         <Card size={"sm"} variant="filled">
             <CardBody >
                 <HStack paddingX={1.5} spacing={4}>
-                    {type === "book" ? (
-                    <>
-                    <Box>
-                        <FaBookOpen size={"1.25rem"} />
-                    </Box>
-                    <Heading maxWidth={400} noOfLines={1} fontSize={"2xl"}>{text}</Heading>
-                    <Spacer/>
-                    <Text maxWidth={400} noOfLines={[1, 2]}>{alttext}</Text>
-                    </>
-                    ) : (
-                    <>
-                    <Box>
-                        <FaQuoteLeft size={"1.25rem"}/>
-                    </Box>
-                    <Text maxWidth={400} noOfLines={[1, 2]}>{text}</Text>
-                    <Spacer/>
-                    <LikeButton liked={liked} handleClick={handleLikeClick}/>
-                    </>  
-                    )}
+                        <Box>
+                            {icon}
+                        </Box>
+                        <Text fontSize={type === "book" ? "xl" : "md"} maxWidth={350} noOfLines={[1, 2]}>{text}</Text>
+                        <Spacer/>
+                        <Heading maxWidth={150} fontSize={"md"} noOfLines={[1, 2]}>{alttext}</Heading>
                 </HStack>
             </CardBody>
         </Card> 
--- quoted-f/src/Components/SearchInput.tsx
+++ quoted-f/src/Components/SearchInput.tsx
@@ -1,9 +1,9 @@
-import { Box, Button, Divider, HStack, IconButton, Input, Kbd, Modal, ModalBody, ModalContent, ModalOverlay, Select, useDisclosure } from "@chakra-ui/react";
+import { Box, HStack, IconButton, Input, Kbd, Modal, ModalBody, ModalContent, ModalOverlay, Select, useDisclosure } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { FaSearch } from "react-icons/fa";
+import { FaBookOpen, FaQuoteLeft, FaSearch } from "react-icons/fa";
+import { Quote } from "../hooks/quoteProvider";
 import APIClient from "../services/api-client";
 import QuoteCard from "./QuoteCard";
-import { Quote } from "../hooks/quoteProvider";
 
 
 const SearchInput = () => {
@@ -82,12 +82,12 @@ const SearchInput = () => {
                                 <Box paddingY={1} paddingX={2} key={index}>
                                     {type === "quote" && 
                                     <QuoteCard
-                                        alttext={""}
                                         key={index}
+                                        icon={<FaQuoteLeft size={"1.25rem"}/>}
                                         type="quote"
-                                        isLiked={false}
                                         id={result?.id}
                                         text={result?.Quote} 
+                                        alttext={result?.AuthorName}
                                     />
                                     
                                     }
@@ -95,7 +95,7 @@ const SearchInput = () => {
                                     <QuoteCard
                                         key={index}
                                         type="book"
-                                        isLiked={false}
+                                        icon={<FaBookOpen size={"1.25rem"} />}
                                         id={result?.id}
                                         alttext={result?.AuthorName}
                                         text={result?.BookName} 
